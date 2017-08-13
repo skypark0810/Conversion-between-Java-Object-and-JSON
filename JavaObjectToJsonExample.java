c9fa5c901be08a2bc971c80bdf2b717ec96ad4a4
skypark0810 <guoleping@gmail.com>
2017-08-13 15:50:10 -0700

    new JSON java file

--- JavaObjectToJsonExample.java
+++ JavaObjectToJsonExample.java
@@ -1,10 +1,10 @@
+package com.Guo.JSON;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.simple.JSONArray;
 import java.io.File;
 import java.io.IOException;
 
@@ -12,21 +12,35 @@ public class JavaObjectToJsonExample {
                                                            
 	public static void main(String[] args) {
 	    
-		// 1. Define your own class' method runConversion() that will implement the conversion
+	     // 1. Define your own class' method runConversion() that will implement the conversion
 	    JavaObjectToJsonExample obj = new JavaObjectToJsonExample();
 		obj.runConversion();                                         
 	}
 
-	    // 2. Define the function of method runConversion()
+	     // 2. Define the function of method runConversion()
 	private void runConversion() {
 	
-	    //3. New a mapper of Jackson
+	     //3. New a mapper of Jackson
 		ObjectMapper mapper = new ObjectMapper();  
         
-        //4. New a Java Object of Employee 
-		Employee employee = createEmployeeObject();                 
+         //4. New a Java Object of Employee 
+		Employee employee = new Employee();  
+		
+		employee.setName("LGuo");
+		employee.setAge(35);
+		employee.setAddress("10 Canadian Road");
+		employee.setPosition("QA");
+		
+		JSONArray skills = new JSONArray();
+		skills.add("Java");
+		skills.add("Selenium");
+		skills.add("Unix/Linux");
+		skills.add("SQL");
 
-		//5. Convert the object (employee) to JSON
+		employee.setSkills(skills);
+		
+
+	     //5. Convert the object (employee) to JSON
 		try {
 			mapper.writeValue(new File("C:\\Employee\\employee.json"), employee);
 
@@ -42,24 +56,52 @@ public class JavaObjectToJsonExample {
 		}
 	}
 
-	private Employee createEmployeeObject() {  
-		Employee employee = new Employee();
-
-		employee.setName("Mike");
-		employee.setAge(30);
-		employee.setAdress("10 Canadian Road");
-		employee.setPosition("QA");
+	public class Employee {  
+		private String name;
+		private int age;
+		private String address;
+		private String position;
+		private JSONArray skills;		
 		
-		List<String> skills = new ArrayList<>();
-		skills.add("Java");
-		skills.add("Selenium");
-		skills.add("Unix/Linux");
-		skills.add("SQL");
+		public String getName(){
+			return name;
+		}
+        
+		public void setName(String name){
+			this.name = name;
+		}
+		
+		public int getAge(){
+			return age;
+		}
 
-		employee.setSkills(skills);
+		public void setAge(int age){
+			this.age = age;
+		}
 
-		return employee;
-
+		public String getAddress(){
+			return address;
+		}
+		
+		public void setAddress(String address){
+			this.address = address;
+		}
+		
+		public String getPosition(){
+			return position;
+		}
+		
+		public void setPosition(String position){
+			this.position = position;
+		}
+				
+		public JSONArray getSkills(){
+			return skills;
+		}
+		
+        public void setSkills(JSONArray skills){
+        	this.skills = skills;
+        }
 	}
 
 }
